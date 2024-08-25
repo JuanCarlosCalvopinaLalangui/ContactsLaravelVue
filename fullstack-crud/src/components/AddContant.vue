2bcd7a5a12386a169443cac55dbbb501bbcab365
Juan Carlos <2172742@my.ipleira.pt>
2024-08-25 04:01:04 +0100

    Feet: Components for list contacts add contacts and menu
    update the app.vue to show  menu component

---
+++ fullstack-crud/src/components/AddContant.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Add Contact</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddContact',
+    // Your component's logic goes here
+}
+</script>
---
+++ fullstack-crud/src/components/HeaderMenu.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">{{title}}</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarColor02"
+          aria-controls="navbarColor02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor02">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/"
+                >Home
+                <span class="visually-hidden">(current)</span>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/add_contant">Add Contacts</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </template>
+
+<script>
+export default {
+  name: 'HeaderMenu', // Ensure this is a multi-word name
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+/* Your styles here */
+</style>
